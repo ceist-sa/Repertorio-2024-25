68fd05340a348fac9282e5a1c5e0ae71264518c6
Simao-Leal <simao.frg.leal@gmail.com>
2026-03-28 13:10:10 +0000

    scores ready for vln II, viola and flutes
    cut pages we are not playing

---
+++ 2025-2026/Mozart - A Flauta Mágica/shorten_scores.sh
@@ -0,0 +1,9 @@
+pdftk originais/violinI.pdf cat 1-33 36-53 64-72 output "partes/VIOLIN I - Mozart - A Flauta Mágica.pdf"
+
+pdftk originais/violinII.pdf cat 1-53 64-70 output "partes/VIOLIN II - Mozart - A Flauta Mágica.pdf"
+
+pdftk originais/viola.pdf cat 1-29 32-47 56-62 output "partes/VIOLA - Mozart - A Flauta Mágica.pdf"
+
+pdftk originais/flute1.pdf cat 1-27 34-38 output "partes/FLUTE I - Mozart - A Flauta Mágica.pdf"
+
+pdftk originais/flute2_piccolo.pdf cat 1-17 20-22 output "partes/FLUTE II - PICCOLO - Mozart - A Flauta Mágica.pdf"
