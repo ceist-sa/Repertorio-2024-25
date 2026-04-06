4f886713a78bbc5f9ea8f60ae13c3edd4bb71273
Simao-Leal <simao.frg.leal@gmail.com>
2026-04-06 16:55:46 +0100

    fixed a bunch of shit

--- 2025-2026/Mozart - A Flauta Mágica/shorten_scores.sh
+++ 2025-2026/Mozart - A Flauta Mágica/shorten_scores.sh
@@ -1,28 +1,43 @@
-pdftk originais/violinI.pdf cat 1-33 36-53 64-72 output "partes/VIOLIN I - Mozart - A Flauta Mágica.pdf"
+pdftk \
+originais-flattened/violinI.pdf \
+cat 1-33 36-53 64-72 \
+output "partes/VIOLIN I - Mozart - A Flauta Mágica.pdf"
 
 pdftk \
-A=originais/violinII.pdf \
+A=originais-flattened/violinII.pdf \
 B=snippets/"VIOLIN II"/replace_page_43.pdf \
 cat A1-42 B A44-53 A64-70 output "partes/VIOLIN II - Mozart - A Flauta Mágica.pdf"
 
-pdftk originais/viola.pdf cat 1-29 32-47 56-62 output "partes/VIOLA - Mozart - A Flauta Mágica.pdf"
+pdftk \
+originais-flattened/viola.pdf \
+cat 1-29 32-47 56-62 \
+output "partes/VIOLA - Mozart - A Flauta Mágica.pdf"
 
-pdftk originais/flute1.pdf cat 1-27 34-38 output "partes/FLUTE I - Mozart - A Flauta Mágica.pdf"
+pdftk \
+A=originais-flattened/flute1.pdf \
+B=snippets/"FLUTE I"/cover-pdfjam.pdf \
+cat B A1-27 A34-38 \
+output "partes/FLUTE I - Mozart - A Flauta Mágica.pdf"
 
-pdftk originais/flute2_piccolo.pdf cat 1-17 20-22 output "partes/FLUTE II - PICCOLO - Mozart - A Flauta Mágica.pdf"
+pdftk \
+A=originais-flattened/flute2_piccolo.pdf \
+B=snippets/"FLUTE II"/cover-pdfjam.pdf \
+cat B A1-17 A20-22 \
+output "partes/FLUTE II - PICCOLO - Mozart - A Flauta Mágica.pdf"
 
 
-pdftk 	A=originais/bassoon1.pdf 
-		B=snippets/"BASSOON I"/cover.pdf 
-		C=snippets/"BASSOON I"/replace_page_8.pdf
-		D=snippets/"BASSOON I"/replace_page_15.pdf
-		E=snippets/"BASSOON I"/replace_page_18.pdf
-		F=snippets/"BASSOON I"/replace_page_20.pdf
-		cat B A1-7 C A9-14 D A16-17 E A19 F A21-end
-		output "partes/BASSOON I - Mozart - A Flauta Mágica.pdf"
+pdftk \
+A=originais-flattened/bassoon1.pdf \
+B=snippets/"BASSOON I"/cover.pdf \
+C=snippets/"BASSOON I"/replace_page_8.pdf \
+D=snippets/"BASSOON I"/replace_page_15.pdf \
+E=snippets/"BASSOON I"/replace_page_18.pdf \
+F=snippets/"BASSOON I"/replace_page_20.pdf \
+cat B A1-7 C A9-14 D A16-17 E A19 F A21-end \
+output "partes/BASSOON I - Mozart - A Flauta Mágica.pdf"
 		
 pdftk \
-A=originais/bassoon2.pdf \
+A=originais-flattened/bassoon2.pdf \
 B=snippets/"BASSOON II"/cover.pdf \
 C=snippets/"BASSOON II"/replace_pages_1-2.pdf \
 D=snippets/"BASSOON II"/replace_page_8.pdf \
@@ -34,7 +49,16 @@ cat B C A3-7 D A9-13 E A15-16 F A18-20 G H A23-end \
 output "partes/BASSOON II - Mozart - A Flauta Mágica.pdf"
 
 pdftk \
-A=originais/cellos_basses.pdf \
+A=originais-flattened/cellos_basses.pdf \
 B=snippets/CELLO/replace_page_4.pdf \
 cat A1-3 B A5-47 A60-66 \
 output "partes/CELLO - Mozart - A Flauta Mágica.pdf"
+
+
+# fix scaling
+pdfjam --paper a4paper --scale 1.1 --outfile "partes/VIOLIN I - Mozart - A Flauta Mágica.pdf" -- "partes/VIOLIN I - Mozart - A Flauta Mágica.pdf"
+pdfjam --paper a4paper --scale 1.1 --outfile "partes/VIOLIN II - Mozart - A Flauta Mágica.pdf" -- "partes/VIOLIN II - Mozart - A Flauta Mágica.pdf"
+pdfjam --paper a4paper --scale 1.1 --outfile "partes/VIOLA - Mozart - A Flauta Mágica.pdf" -- "partes/VIOLA - Mozart - A Flauta Mágica.pdf"
+pdfjam --paper a4paper --scale 1.1 --outfile "partes/CELLO - Mozart - A Flauta Mágica.pdf" -- "partes/CELLO - Mozart - A Flauta Mágica.pdf"
+pdfjam --paper a4paper --scale 1.1 --outfile "partes/FLUTE I - Mozart - A Flauta Mágica.pdf" -- "partes/FLUTE I - Mozart - A Flauta Mágica.pdf"
+pdfjam --paper a4paper --scale 1.1 --outfile "partes/FLUTE II - PICCOLO - Mozart - A Flauta Mágica.pdf" -- "partes/FLUTE II - PICCOLO - Mozart - A Flauta Mágica.pdf"
